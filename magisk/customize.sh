91798934edefc2146b92400f8cd8fc7a9ec1cf33
simo8102 <45913291+simo8102@users.noreply.github.com>
2024-09-05 22:16:54 +0800

    Update customize.sh

--- magisk/customize.sh
+++ magisk/customize.sh
@@ -3,5 +3,3 @@
 #You can delete these by execute the following command:
 #adb shell settings delete global pps_oaid
 #adb shell settings delete global pps_track_limit
-settings put global pps_oaid 00000000-0000-0000-0000-000000000000
-settings put global pps_track_limit true
